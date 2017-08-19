811230c81696296171177754b604d734b420aad0
Boris D. <boris.dob@gmail.com>
2017-08-19 12:15:26 -0700

    Set up navigation.

--- XamFormsEtudes.Mac/AppDelegate.fs
+++ XamFormsEtudes.Mac/AppDelegate.fs
@@ -10,7 +10,7 @@ open Xamarin.Forms.Platform.MacOS
 [<Register ("AppDelegate")>]
 type AppDelegate () =
     inherit FormsApplicationDelegate ()
-    let rect = new CoreGraphics.CGRect (50.0, 50.0, 800.0, 600.0)
+    let rect = new CoreGraphics.CGRect (50.0, 50.0, 600.0, 400.0)
     let style = NSWindowStyle.Closable ||| NSWindowStyle.Resizable ||| NSWindowStyle.Titled
     let window = new NSWindow (rect, style, NSBackingStore.Buffered, false)
     do
--- XamFormsEtudes/XamFormsEtudes.fs
+++ XamFormsEtudes/XamFormsEtudes.fs
@@ -2,10 +2,25 @@
 
 open Xamarin.Forms
 
-type App() = 
-    inherit Application()
-    let stack = StackLayout(VerticalOptions = LayoutOptions.Center)
-    let label = Label(XAlign = TextAlignment.Center, Text = "Welcome to F# Xamarin.Forms!")
-    do 
-        stack.Children.Add(label)
-        base.MainPage <- ContentPage(Content = stack)
+module ReadFileResource =
+    type Page () =
+        inherit ContentPage ()
+        let stack = StackLayout (VerticalOptions = LayoutOptions.Center)
+        let label = Label (XAlign = TextAlignment.Center, Text = "Welcome to F# Xamarin.Forms!")
+        do 
+            stack.Children.Add (label)
+            base.Content <- stack
+
+
+type App () = 
+    inherit Application ()
+    let stack = StackLayout (VerticalOptions = LayoutOptions.Center, Padding = Thickness (40.0))
+    let label = Label (XAlign = TextAlignment.Center, Text = "Main Page")
+    let button = Button (Text = "Read File Resource")
+    let mainPage = ContentPage (Content = stack)
+    let nextPage = ReadFileResource.Page ()
+    do
+        button.Clicked.Add (fun arg -> mainPage.Navigation.PushAsync (nextPage) |> ignore)
+        stack.Children.Add (label)
+        stack.Children.Add (button)
+        base.MainPage <- NavigationPage (mainPage)
