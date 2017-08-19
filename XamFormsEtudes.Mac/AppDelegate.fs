64f87df728ead6245805f451d597c4fa8dcda8ed
Boris D. <boris.dob@gmail.com>
2017-08-19 10:17:16 -0700

    Init project for Mac, iOS, and Android.

---
+++ XamFormsEtudes.Mac/AppDelegate.fs
@@ -0,0 +1,26 @@
+﻿namespace XamFormsEtudes.Mac
+
+open Foundation
+open AppKit
+
+open System
+open Xamarin.Forms
+open Xamarin.Forms.Platform.MacOS
+
+[<Register ("AppDelegate")>]
+type AppDelegate () =
+    inherit FormsApplicationDelegate ()
+    let rect = new CoreGraphics.CGRect (50.0, 50.0, 800.0, 600.0)
+    let style = NSWindowStyle.Closable ||| NSWindowStyle.Resizable ||| NSWindowStyle.Titled
+    let window = new NSWindow (rect, style, NSBackingStore.Buffered, false)
+    do
+        window.Title <- "Xamarin.Forms Etudes"
+        window.TitleVisibility <- NSWindowTitleVisibility.Hidden
+
+    override __.MainWindow with get () = window
+
+    override self.DidFinishLaunching notification =
+        do
+            Forms.Init ()
+            self.LoadApplication (XamFormsEtudes.App ())
+            base.DidFinishLaunching (notification)
---
+++ XamFormsEtudes.Mac/Main.fs
@@ -0,0 +1,10 @@
+﻿namespace XamFormsEtudes.Mac
+open System
+open AppKit
+
+module main =
+    [<EntryPoint>]
+    let main args =
+        NSApplication.Init ()
+        NSApplication.Main (args)
+        0
