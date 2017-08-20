721b79040758255bedb812cdac897869e06c7048
Boris D. <boris.dob@gmail.com>
2017-08-20 15:28:34 -0700

    Refactor.

--- XamFormsEtudes/XamFormsEtudes.fs
+++ XamFormsEtudes/XamFormsEtudes.fs
@@ -2,21 +2,25 @@
 
 open Xamarin.Forms
 
+type ReadFileResourcePage (mainPage : ContentPage) =
+    let viewModel = ReadFileResource.PageVM ()
+    let button = Button (Text = "Read File Resource")
+    let page = ReadFileResource.Page (viewModel)
+    let loadFilePage _ = do
+        mainPage.Navigation.PushAsync (page) |> ignore
+        viewModel.Text <- "Sample.txt" |> ReadFileResource.readFile
+    do
+        button.Clicked.Add (loadFilePage)
+    member val Button = button with get
 
 type App () = 
     inherit Application ()
     let stack = StackLayout (VerticalOptions = LayoutOptions.Center, Padding = Thickness (40.0))
     let label = Label (XAlign = TextAlignment.Center, Text = "Main Page")
-    let button = Button (Text = "Read File Resource")
     let mainPage = ContentPage (Content = stack)
-    let readFilePageVM = ReadFileResource.PageVM ()
-    let readFilePage = ReadFileResource.Page (readFilePageVM)
-    let loadFilePage _ = do
-        mainPage.Navigation.PushAsync (readFilePage) |> ignore
-        //ReadFileResource.printAssemblyResources ()
-        readFilePageVM.Text <- "Sample.txt" |> ReadFileResource.readFile 
+    let page1 = ReadFileResourcePage (mainPage)
     do
-        button.Clicked.Add (loadFilePage)
+
         stack.Children.Add (label)
-        stack.Children.Add (button)
+        stack.Children.Add (page1.Button)
         base.MainPage <- NavigationPage (mainPage)
