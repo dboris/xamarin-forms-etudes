855bd58f2a6992a72a0f03cd36c5f8b462486500
Boris D. <boris.dob@gmail.com>
2017-08-29 17:02:41 -0700

    Improve null checking.

--- XamFormsEtudes.Mac/CustomRenderers.fs
+++ XamFormsEtudes.Mac/CustomRenderers.fs
@@ -30,7 +30,7 @@ module CustomRenderers =
         override self.OnElementChanged (e : ElementChangedEventArgs<HybridWebView>) =
             base.OnElementChanged e
 
-            if self.Control = null
+            if isNull self.Control
             then 
                 let script = new WKUserScript (new NSString (javaScriptFunction), WKUserScriptInjectionTime.AtDocumentEnd, false)
                 userController <- new WKUserContentController ()
@@ -40,13 +40,13 @@ module CustomRenderers =
                 let webView = new WKWebView (self.Frame, config)
                 self.SetNativeControl webView
 
-            if not <| obj.ReferenceEquals (e.OldElement, null)  // e.OldElement <> null
+            if not (isNull <| box e.OldElement)  // e.OldElement <> null
             then
                 userController.RemoveAllUserScripts ()
                 userController.RemoveScriptMessageHandler "invokeAction"
                 e.OldElement.Cleanup ()
 
-            if not <| obj.ReferenceEquals (e.NewElement, null)  // e.NewElement <> null
+            if not (isNull <| box e.NewElement)  // e.NewElement <> null
             then
                 let fileName = IO.Path.Combine (NSBundle.MainBundle.BundlePath, "Contents", "Resources", self.Element.Uri)
                 let request = new NSUrlRequest (new NSUrl (fileName, false))
